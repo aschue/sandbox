94d43f47ccbac3927d49756deee5791846ee2251
Anna S <annateich@gmail.com>
2021-01-22 16:16:15 +0100

    Buxfix and added comments

--- website_image_scraping.py
+++ website_image_scraping.py
@@ -4,6 +4,7 @@ Created on Sun Dec 27 11:06:06 2020
 
 @author: Anna
 """
+# saving the current image from a website every few seconds
 
 import io
 import requests
@@ -12,13 +13,16 @@ from datetime import datetime
 import time
 
 
-def persist_image(file_path:str,url:str):
+# function to save image
+def get_image(file_path:str,url:str):
+    
+    # getting image using the requests library 
     try:
         image_content = requests.get(url).content
-
     except Exception as e:
         print(f"ERROR - Could not download {url} - {e}")
 
+    # get image content and save as jpeg
     try:
         image_file = io.BytesIO(image_content)
         image = Image.open(image_file).convert('RGB')
@@ -29,16 +33,11 @@ def persist_image(file_path:str,url:str):
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
         
-        
-        
+# image address                
 url = "http://192.168.0.59/jpg/image.jpg"
-datetime.now().strftime("%d%m%y_%H%M%S")
 
-
-file_name = 'Scraping/' + datetime.now().strftime("%d%m%y_%H%M%S")
-persist_image('Scraping/test', url)
-
+# loop to save image every 2 seconds until 2030
 while datetime.now().year < 2030: 
     file_name = 'Scraping/' + datetime.now().strftime("%d%m%y_%H%M%S")
-    persist_image(file_name, url)
+    get_image(file_name, url)
     time.sleep(2)
